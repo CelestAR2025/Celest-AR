ceac90e865c29091f778eb667ebaa774ca58052e
CelestAR2025 <celestar2526@gmail.com>
2025-10-08 22:35:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Celest-AR/',  
   esbuild: {
     jsx: 'automatic', // O kaya `transform` depende sa error
   },
